0480c7d9d25c6cff8da5f3b3aff6c9b86b24cb64
Afsal Backer <63408724+afsal-backer@users.noreply.github.com>
2021-10-03 14:05:44 +0530

    Update home-page.js

--- test/pages/home-page.js
+++ test/pages/home-page.js
@@ -1,21 +1,28 @@
+//Page object to store all Homepage related webelements and helper methods
+
 class Homepage{
 
+    //method to open browser and navigate to default url
     openUrl(){
         return browser.url('/')
     }
 
+    //method to verify Trending section header text is displayed
     verifyTrendingSection(){
         return $('//h3[text()="Trending"]');
     }
-
+    
+    //method to return search bar weblement
     verifySearchBar(){
         return $('//div[@id="searchbar"]');
     }
 
+    //method to return search bar input textbox webelement
     typeInSearchBar(){
         return $('//div[@id="searchbar"]//input');
     }
 
+    //method to return search button webelement
     clickSearchButton(){
         return $('//a[contains(@class,"SearchButton")]');
     }
