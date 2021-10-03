3072a3f1431dd9bf42bf2a03e459fdac1e5cf79a
Afsal Backer <63408724+afsal-backer@users.noreply.github.com>
2021-10-03 14:06:32 +0530

    Update search-page.js

--- test/pages/search-page.js
+++ test/pages/search-page.js
@@ -1,5 +1,8 @@
+//Page object to store all Search page related webelements and helper methods
+
 class Searchpage{
 
+    //method to return webelement of search result header title
     getSearchResultHeader(){
         return $('//h1[contains(@class,"Title-sc")]');
     }
