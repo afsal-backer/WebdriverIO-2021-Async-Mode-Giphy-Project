e106234397ac3801567644c89eaa1b7998c5c60e
Afsal Backer <63408724+afsal-backer@users.noreply.github.com>
2021-10-03 14:07:59 +0530

    Update trending-page.js

--- test/pages/trending-page.js
+++ test/pages/trending-page.js
@@ -1,9 +1,13 @@
+//Page object to store all Trending page related webelements and helper methods
+
 class Trendingpage{
 
+    //method to return webelement of first available gif
     clickOnGif(){
         return $('(//div[contains(@class,"ListWrapper")]//li//div[contains(@class,"Container-sc")])[1]');
     }
 
+    //method to return weblement of the name of the opened gif
     getGifName(){
         return $('//div[contains(@class,"Content-sc")]//h1');
     }
